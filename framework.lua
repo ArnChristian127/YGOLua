5aa48cd531ca1676d453b6450d250ddf102b184e
Arn <80439628+ArnChristian127@users.noreply.github.com>
2025-03-12 20:27:09 +0800

    rework

--- framework.lua
+++ framework.lua
@@ -1,36 +1,54 @@
-local Effects = {
-    EFFECT_SPSUMMON_SELF_FROMHAND = 0
+oncreate = {
+    card_effect = function(self, card, datas)
+        datas = datas or {}
+        local eff = Effect.CreateEffect(card)
+        if datas.category then eff:SetCategory(datas.category) end
+        if datas.efftype then eff:SetType(datas.efftype) end
+        if datas.range then eff:SetRange(datas.range) end
+        if datas.opt then eff:SetCountLimit(datas.opt) end
+        if datas.target then eff:SetTarget(datas.target) end
+        if datas.operation then eff:SetOperation(datas.operation) end
+        card:RegisterEffect(eff)
+    end
 }
-
-local Events = {
-    EVENT_IGNITION = 0
-}
-local Count = {
-    SOFT_ONCEPER_TURN = 0,
-    HARD_ONCEPER_TURN = 1
-}
-Call = {}
-
-function Call.Condition(str)
-end
-
-function Call.Target(str)
-    local lists = {
-        ["special-summon"] = function(e,tp,eg,ep,ev,re,r,rp,chk)
-            if chk==0 then
-                return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false)
+function sp_target(access, check, opinfo)
+    local dataSets = {
+        ["selfsp-target"] = function(e,tp,eg,ep,ev,re,r,rp,chk)
+            if chk == 0 then
+                return Duel.GetLocationCount(tp,LOCATION_MZONE) > 0 and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false)
             end
-            Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+            Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, e:GetHandler(), 1, 0, 0)
+        end,
+        ["sp-target"] = function(e,tp,eg,ep,ev,re,r,rp,chk)
+            if chk == 0 then
+                for _, v in ipairs(check or {}) do
+                    if v then return true end
+                end
+                return false
+            end
+            if opinfo then
+                for _, info in ipairs(opinfo) do
+                    Duel.SetOperationInfo(0, table.unpack(info))
+                end
+            end
         end
     }
-    local target = lists[str]
-    return target
+    return dataSets[access] or function() end
 end
 
-function Call.Operation(str)
-    local lists = {
-        ["special-summon"] = function(e,tp,eg,ep,ev,re,r,rp)
+function sp_operation(access)
+    local dataSets = {
+        ["selfsp-operation"] = function(e,tp,eg,ep,ev,re,r,rp,chk)
             local c=e:GetHandler()
+            if c:IsRelateToEffect(e) then
+                Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+            end
+        end
+    }
+    local res = dataSets[access]
+    return res
+end
+
             if c:IsRelateToEffect(e) then
                 Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
             end
