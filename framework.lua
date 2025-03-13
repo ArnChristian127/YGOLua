93970fe5d30c8ffabc903040766ce4152d21c050
Arn <80439628+ArnChristian127@users.noreply.github.com>
2025-03-13 09:52:03 +0800

    Update framework.lua

--- framework.lua
+++ framework.lua
@@ -1,50 +1,102 @@
-oncreate = {
-    card_effect = function(self, card, datas)
-        datas = datas or {}
-        local eff = Effect.CreateEffect(card)
-        if datas.category then eff:SetCategory(datas.category) end
-        if datas.efftype then eff:SetType(datas.efftype) end
-        if datas.range then eff:SetRange(datas.range) end
-        if datas.opt then eff:SetCountLimit(datas.opt) end
-        if datas.target then eff:SetTarget(datas.target) end
-        if datas.operation then eff:SetOperation(datas.operation) end
-        card:RegisterEffect(eff)
+onCreate={
+    CardEffect=function(self,card,datasets) 
+        local effectMethods={
+            description="SetDescription",
+            category="SetCategory",
+            type="SetType",
+            range="SetRange",
+            count="SetCountLimit",
+            condition="SetCondition",
+            target="SetTarget",
+            operation="SetOperation",
+        }
+        for _,datas in ipairs(datasets) do
+            local eff=Effect.CreateEffect(card)
+            for key,method in pairs(effectMethods) do
+                if datas[key] then
+                    eff[method](eff,datas[key])
+                end
+            end
+            card:RegisterEffect(eff)
+        end
     end
 }
-function sp_target(access, check, opinfo)
-    local dataSets = {
-        ["selfsp-target"] = function(e,tp,eg,ep,ev,re,r,rp,chk)
+--special summon--
+sp={
+    selfcheck=function(self, check, opinfo)
+        return function(e,tp,eg,ep,ev,re,r,rp,chk)
             if chk == 0 then
-                return Duel.GetLocationCount(tp,LOCATION_MZONE) > 0 and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false)
-            end
-            Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, e:GetHandler(), 1, 0, 0)
-        end,
-        ["sp-target"] = function(e,tp,eg,ep,ev,re,r,rp,chk)
-            if chk == 0 then
-                for _, v in ipairs(check or {}) do
-                    if v then return true end
+                for _, v in pairs(check) do
+                    if type(v) == "function" then
+                        if v(e,tp,eg,ep,ev,re,r,rp) then return true end
+                    elseif v then
+                        return true
+                    end
                 end
                 return false
             end
-            if opinfo then
-                for _, info in ipairs(opinfo) do
-                    Duel.SetOperationInfo(0, table.unpack(info))
+            if opinfo and type(opinfo) == "table" then
+                for _, v in ipairs(opinfo) do
+                    local info = type(v) == "function" and v(e,tp,eg,ep,ev,re,r,rp) or v
+                    if type(info) == "table" then
+                        Duel.SetOperationInfo(0, table.unpack(info))
+                    end
                 end
             end
         end
-    }
-    return dataSets[access] or function() end
-end
-
-function sp_operation(access)
-    local dataSets = {
-        ["selfsp-operation"] = function(e,tp,eg,ep,ev,re,r,rp,chk)
+    end,
+    selfactivation=function(self, next, nextbreak)
+        return function(e,tp,eg,ep,ev,re,r,rp)
             local c=e:GetHandler()
             if c:IsRelateToEffect(e) then
-                Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+                local res=Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+                if res>0 then
+                    if nextbreak == true then Duel.BreakEffect() end
+                    next(e,tp,eg,ep,ev,re,r,rp)
+                end
             end
         end
-    }
-    local res = dataSets[access]
-    return res
+    end
+}
+--common effects--
+put = {
+    TOHAND = 0,
+    TOGRAVE = 1,
+    TODECK = 2,
+    REMOVE = 3
+}
+function search(query, types, y, o, min, max)
+    return function(e, tp, eg, ep, ev, re, r, rp)
+        local maps = {
+            [put.TOHAND] = HINTMSG_ATOHAND,
+            [put.TOGRAVE] = HINTMSG_TOGRAVE,
+            [put.TODECK] = HINTMSG_TODECK,
+            [put.REMOVE] = HINTMSG_REMOVE
+        }
+        if type(types) ~= "table" then
+            types = {types} 
+        end
+        local getMSG = nil
+        for _, v in ipairs(types) do
+            if maps[v] then
+                getMSG = maps[v]
+                break
+            end
+        end
+        Duel.Hint(HINT_SELECTMSG,tp,getMSG)
+        local g=Duel.SelectMatchingCard(tp,query,tp,y,o,min,max,nil)
+        if #g>0 then
+            local action = types[1]
+            if action==put.TOHAND then
+                Duel.SendtoHand(g,nil,REASON_EFFECT)
+                Duel.ConfirmCards(1-tp,g)
+            elseif action==put.TOGRAVE then
+                Duel.SendtoGrave(g,REASON_EFFECT)
+            elseif action==put.TODECK then
+                Duel.SendtoDeck(g,nil,2,REASON_EFFECT)
+            elseif action==put.REMOVE then
+                Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
+            end
+        end
+    end
 end
